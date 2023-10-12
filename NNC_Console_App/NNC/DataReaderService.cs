604691d5a950fea92f79ece7fdf05330bec8952f
AnthonyBaruch <anthony@baruchstudio.co.za>
2023-10-12 09:31:40 +0200

    Resolve Registration via dedicated method

--- NNC_Console_App/NNC/DataReaderService.cs
+++ NNC_Console_App/NNC/DataReaderService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Text;
+﻿using System.Text;
 
 namespace NNC
 {
@@ -18,16 +17,7 @@ namespace NNC
                         {
                             var vehicleData = new VehicleKdTreeNode();
                             vehicleData.VehicleId = reader.ReadInt32();
-
-                            // Read and populate the null-terminated ASCII string
-                            StringBuilder registrationBuilder = new StringBuilder();
-                            char nextChar;
-                            while ((nextChar = reader.ReadChar()) != '\0')
-                            {
-                                registrationBuilder.Append(nextChar);
-                            }
-                            vehicleData.VehicleRegistration = registrationBuilder.ToString();
-
+                            vehicleData.VehicleRegistration = ReadNullTerminatedString(reader);
                             vehicleData.Latitude = reader.ReadSingle();
                             vehicleData.Longitude = reader.ReadSingle();
                             vehicleData.RecordedTimeUTC = reader.ReadUInt64();
@@ -42,53 +32,7 @@ namespace NNC
                 Console.WriteLine("An error occurred: " + e.Message);
             }
             return vehicleList;
-        }
-
-            private static void ReadChunk(ConcurrentBag<VehicleKdTreeNode> data, FileStream fs, long start, long end)
-        {
-            try
-            {
-                using (BinaryReader reader = new BinaryReader(fs, Encoding.ASCII, true))
-                {
-                    while (reader.PeekChar() != -1)
-                    {
-
-                        fs.Seek(start, SeekOrigin.Begin);
-
-                        if (end > fs.Length)
-                        {
-                            end = fs.Length;
-                        }
-
-                        while (fs.Position < end)
-                        {
-                            try
-                            {
-                                var vehicleData = new VehicleKdTreeNode
-                                {
-                                    VehicleId = reader.ReadInt32(),
-                                    VehicleRegistration = ReadNullTerminatedString(reader),
-                                    Latitude = reader.ReadSingle(),
-                                    Longitude = reader.ReadSingle(),
-                                    RecordedTimeUTC = reader.ReadUInt64()
-                                };
-
-                                data.Add(vehicleData);
-                            }
-                            catch (Exception e)
-                            {
-
-                                Console.WriteLine($"Failed to create node: Position: {fs.Position}, VehicleId: {ReadNullTerminatedString(reader)} | " + e.Message);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"An error occured in ReadChunk(Start: {start}, End: {end}): " + e.Message);
-            }
-        }
+        }            
 
         private static string ReadNullTerminatedString(BinaryReader reader)
         {
