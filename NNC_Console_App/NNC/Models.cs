254878ab28d4c2afcac47d22bc60247a7c473d20
AnthonyBaruch <anthony@baruchstudio.co.za>
2023-10-12 08:23:43 +0200

    API - Created Models

---
+++ NNC_Console_App/NNC/Models.cs
@@ -0,0 +1,23 @@
+﻿namespace NNC
+{
+    class Models
+    {
+        public class VehicleKdTreeNode
+        {
+            public int VehicleId;
+            public string VehicleRegistration;
+            public float Latitude;
+            public float Longitude;
+            public ulong RecordedTimeUTC;
+            public VehicleKdTreeNode Left;
+            public VehicleKdTreeNode Right;
+        }
+
+        public class TargetCoordinate
+        {
+            public int Position;
+            public float Latitude;
+            public float Longitude;
+        }
+    }
+}
