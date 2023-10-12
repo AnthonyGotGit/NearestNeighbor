22a06758390a7a5cc7bea5c0e724ed29f3000e6a
AnthonyBaruch <anthony@baruchstudio.co.za>
2023-10-12 08:40:06 +0200

    Rebuilt solution, Created Data Reader Service

--- NNC_Console_App/NNC/Models.cs
+++ NNC_Console_App/NNC/Models.cs
@@ -1,23 +1,20 @@
-﻿namespace NNC
+﻿namespace ModelsNamespace
 {
-    class Models
+    public class VehicleKdTreeNode
     {
-        public class VehicleKdTreeNode
-        {
-            public int VehicleId;
-            public string VehicleRegistration;
-            public float Latitude;
-            public float Longitude;
-            public ulong RecordedTimeUTC;
-            public VehicleKdTreeNode Left;
-            public VehicleKdTreeNode Right;
-        }
+        public int VehicleId;
+        public string VehicleRegistration;
+        public float Latitude;
+        public float Longitude;
+        public ulong RecordedTimeUTC;
+        public VehicleKdTreeNode Left;
+        public VehicleKdTreeNode Right;
+    }
 
-        public class TargetCoordinate
-        {
-            public int Position;
-            public float Latitude;
-            public float Longitude;
-        }
+    public class TargetCoordinate
+    {
+        public int Position;
+        public float Latitude;
+        public float Longitude;
     }
 }
--- NNC_Console_App/NNC/Program.cs
+++ NNC_Console_App/NNC/Program.cs
@@ -1,6 +1,31 @@
-﻿class Program
+﻿using ModelsNamespace;
+using NNC;
+
+class Program
 {
     static void Main(string[] args)
     {
+        string dataFilePath = "C:\\Dev\\Personal\\NNC\\NNC_Console_App\\NNC\\Data\\VehiclePositions.dat";
+
+        var totalTimer = new System.Diagnostics.Stopwatch();
+        var dataReadTimer = new System.Diagnostics.Stopwatch();
+        var closestNodeTimer = new System.Diagnostics.Stopwatch();
+
+        totalTimer.Start();
+
+        // Check if data file is empty
+        FileInfo fileInfo = new FileInfo(dataFilePath);
+        if (fileInfo.Length == 0)
+        {
+            Console.WriteLine("The file is empty.");
+        }
+
+        Console.WriteLine("Starting to read binary file...");
+        dataReadTimer.Start();
+
+        // Read Binary Data
+        List<VehicleKdTreeNode> vehicleList = DataReaderService.ReadBinaryData(dataFilePath);
+
+        dataReadTimer.Stop();
     }
 }
